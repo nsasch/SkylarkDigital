52891fe392e1caa62168ab7fe9a168636140c150
Nicholas Asch <github@pilotsworld.net>
2020-01-28 22:15:59 -0800

    time syncing

--- esp32/sketch_jan25b/sketch_jan25b.ino
+++ esp32/sketch_jan25b/sketch_jan25b.ino
@@ -1,18 +1,36 @@
 #include <WiFi.h>
+#include <esp_wifi.h>
 #include <AsyncUDP.h>
 
 const char *ssid = "esp32-00";
 const char *password = "72sjwscrkd8";
+#define MAX_SAMPLES 20
+#define RX_PIN 22
+#define TX_PIN 23
 
 AsyncUDP udp;
-int32_t rtt = 0, clock_offset = 0;
-uint8_t avg_ct = 0;
-#define SAMPLE_CT 20
-int32_t samples[SAMPLE_CT][2] = {
+int64_t clock_offset = 0;
+
+uint8_t sample_ct = 0;
+uint32_t t0 = 0;
+int64_t samples[MAX_SAMPLES][2] = {
+  {0,0},{0,0},{0,0},{0,0},{0,0},
+  {0,0},{0,0},{0,0},{0,0},{0,0},
   {0,0},{0,0},{0,0},{0,0},{0,0},
   {0,0},{0,0},{0,0},{0,0},{0,0}
 };
 bool waiting_for_server = false;
+volatile uint32_t isr_micros = 0;
+volatile bool expecting_isr = false;
+void IRAM_ATTR isr() {
+  if (!expecting_isr) {
+    // ignore unexpected ISRs
+    return;
+  }
+  isr_micros = micros();
+  expecting_isr = false;
+  digitalWrite(TX_PIN, LOW);
+}
 
 template<typename T>
 int cmp(const void *arg1, const void *arg2) {
@@ -23,16 +41,28 @@ int cmp(const void *arg1, const void *arg2) {
   return 0;
 }
 
-void setup() {
-  Serial.begin(115200);
-  WiFi.disconnect(true);
-  WiFi.mode(WIFI_STA);
+void init_wifi() {
+  Serial.println("Connecting to WiFi");
   WiFi.begin(ssid, password);
   delay(400);
   while (WiFi.status() != WL_CONNECTED) {
     Serial.println("WiFi Failed");
     delay(100);
   }
+  Serial.println("Connected");
+}
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.disconnect(true);
+  WiFi.mode(WIFI_STA);
+  esp_wifi_set_ps(WIFI_PS_NONE);
+  init_wifi();
+
+  pinMode(RX_PIN, INPUT_PULLUP);
+  pinMode(TX_PIN, OUTPUT);
+  digitalWrite(TX_PIN, HIGH);
+  attachInterrupt(RX_PIN, isr, FALLING);
 
   if (udp.connect(WiFi.gatewayIP(), 1234)) {
     Serial.print("UDP Listening on IP: ");
@@ -42,62 +72,76 @@ void setup() {
       uint32_t t_server = *((uint32_t*) packet.data());
 
       if (packet.length() != 4) {
+        Serial.println("ERR bad packet length");
         return;
       }
       if (!waiting_for_server) {
+        Serial.println("ERR unexpected packet");
         return;
       }
+      waiting_for_server = false;
+      if (isr_micros) {
+        digitalWrite(TX_PIN, HIGH);
+        //Serial.printf("C->S time, S->C time: %d\t %d\n", isr_micros-t0, t_local-isr_micros);
+        //Serial.printf("t_server when server sent ISR: %d\n", t_server);
+        //Serial.printf("t_local when received ISR: %d\n", isr_micros);
+        //Serial.printf("assume 10us ISR delay, t_local + clock_offset should equal t_server + 10us\n");
+        Serial.printf("delta: %lld\t (%lld, %d)\n", (isr_micros + clock_offset) - (t_server + 10), isr_micros + clock_offset, t_server + 10);
+        
+        isr_micros = 0;
+      }
+
+      // t0
+      //samples[sample_ct][0] = -t0/2;
+      //samples[sample_ct][1] = -t0;
 
       // t1
-      //clock_offset += t_server/2;
-      //rtt += t_server;
+      //samples[sample_ct][0] += t_server/2;
+      //samples[sample_ct][1] += t_server;
 
       // t2
-      //clock_offset += t_server/2;
-      //rtt -= t_server;
-      
-      clock_offset += t_server;
+      //samples[sample_ct][0] += t_server/2;
+      //samples[sample_ct][1] -= t_server;
 
       // t3
-      clock_offset -= t_local/2;
-      rtt += t_local;
+      //samples[sample_ct][0] -= t_local/2;
+      //samples[sample_ct][1] += t_local;
 
-      //Serial.printf("offset, rtt %d\t %d\n", clock_offset, rtt);
-      samples[avg_ct][0] = clock_offset;
-      samples[avg_ct][1] = rtt;
-      avg_ct++;
-      if (avg_ct == SAMPLE_CT) {
-        avg_ct = 0;
+      samples[sample_ct][0] = t_server - t_local/2 - t0/2;
+      samples[sample_ct][1] = t_local - t0;
+
+      //Serial.printf("t0, t1&t2, t3: %u\t %u\t %u\n", t0, t_server, t_local);
+
+      sample_ct++;
+      if (sample_ct == MAX_SAMPLES) {
+        sample_ct = 0;
+        uint32_t rtt = 0;
+        qsort(&samples, MAX_SAMPLES, sizeof(int64_t[2]), cmp<int64_t[2]>);
         clock_offset = 0;
-        rtt = 0;
-        qsort(&samples, SAMPLE_CT, sizeof(int32_t[2]), cmp<int32_t[2]>);
-        for (int i=SAMPLE_CT/2-2; i<SAMPLE_CT/2+2; i++) {
+        for (int i=0; i<4; i++) {
           clock_offset += samples[i][0]/4;
           rtt += samples[i][1]/4;
         }
-        for (int i=0; i<SAMPLE_CT; i++) {
-          Serial.printf("offset, rtt %d\t %d\n", samples[i][0], samples[i][1]);
-        }
-        Serial.printf("AVG offset, rtt %d\t %d\n", clock_offset, rtt);
+        Serial.println("Average of 4 lowest RTTs of last 20 samples");
+        Serial.printf("offset, rtt %lld\t %u\n", clock_offset, rtt);
       }
-      waiting_for_server = false;
     });
   }
 }
 
 void loop() {
-  uint32_t t;
+  if (WiFi.status() != WL_CONNECTED) {
+    init_wifi();
+  }
   if (waiting_for_server) {
-    delay(10);
+    delay(1);
   } else {
-    delay(50);
+    delay(5);
 
-    t = micros();
-    udp.write((uint8_t*) (&t), 4);
+    t0 = micros();
 
+    udp.write((uint8_t*) (&t0), 4);
+    expecting_isr = true;
     waiting_for_server = true;
-    // t0
-    clock_offset = -t/2;
-    rtt = -t;
   }
 }
--- esp32/time_server/time_server.ino
+++ esp32/time_server/time_server.ino
@@ -4,32 +4,61 @@
 
 const char *ssid = "esp32-00";
 const char *password = "72sjwscrkd8";
+#define RX_PIN 22
+#define TX_PIN 23
 
 AsyncUDP udp;
-
-void setup()
-{
-    Serial.begin(115200);
-    WiFi.disconnect(true);
-    WiFi.softAP(ssid, password);
-    Serial.print("IP address: ");
-    Serial.println(WiFi.softAPIP());
-
-    if (udp.listen(1234)) {
-        Serial.print("UDP Listening on IP: ");
-        Serial.println(WiFi.localIP());
-        udp.onPacket([](AsyncUDPPacket packet) {
-            uint32_t now = micros();
-            /*if (packet.length() != 4) {
-              return;
-            }*/
-            
-            packet.write((uint8_t*) &now, 4);
-        });
-    }
+volatile uint32_t isr_micros = 0;
+volatile bool expecting_isr = false;
+void IRAM_ATTR isr() {
+  if (!expecting_isr) {
+    // ignore unexpected ISRs
+    return;
+  }
+  isr_micros = micros();
+  expecting_isr = false;
+  digitalWrite(TX_PIN, HIGH);
 }
 
-void loop()
-{
-    delay(1000);
+uint32_t packet_recv_micros = 0;
+void setup() {
+  Serial.begin(115200);
+  WiFi.disconnect(true);
+  WiFi.softAP(ssid, password);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.softAPIP());
+
+  pinMode(RX_PIN, INPUT_PULLUP);
+  pinMode(TX_PIN, OUTPUT);
+  digitalWrite(TX_PIN, HIGH);
+  attachInterrupt(RX_PIN, isr, FALLING);
+
+  if (udp.listen(1234)) {
+    Serial.print("UDP Listening on IP: ");
+    Serial.printf("CONFIG_ESP32_RTC_CLOCK_SOURCE_INTERNAL_8MD256: %d\n", CONFIG_ESP32_RTC_CLOCK_SOURCE_INTERNAL_8MD256);
+    Serial.println(WiFi.localIP());
+    udp.onPacket([](AsyncUDPPacket packet) {
+      packet_recv_micros = micros();
+      isr_micros = 0;
+      expecting_isr = true;
+      digitalWrite(TX_PIN, LOW);
+      packet.write((uint8_t*) &packet_recv_micros, 4);
+
+      const int32_t t_low = micros() - packet_recv_micros;
+      if (t_low < 500) {
+        // leave TX low for ~500us
+        delayMicroseconds(500 - t_low);
+      }
+      digitalWrite(TX_PIN, HIGH);
+    });
+  }
+}
+
+void loop() {
+  if (isr_micros) {
+    Serial.printf("Sent/Recv ISR: %u (%u->%u)\n", isr_micros - packet_recv_micros, packet_recv_micros, isr_micros);
+    isr_micros = 0;
+  } else {
+    delay(1);
+  }
 }
